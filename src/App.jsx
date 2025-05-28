c2df8483e8422e40106275a377e68316c08897ed
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-05-28 22:19:15 +0700

    Add dark light #1

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,22 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import AlarmIcon from '@mui/icons-material/Alarm'
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 import Typography from '@mui/material/Typography'
+
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')}>
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>dathuynh</div>
       <Typography variant='body2' color='text.secondary'>
         Hello dathuynh
--- src/theme.js
+++ src/theme.js
@@ -1,22 +1,19 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
-
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { deepOrange, orange, cyan, teal } from '@mui/material/colors'
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#556cd6'
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
     },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-      secondary: red[500]
+    dark: {
+      palette: {
+        main: cyan,
+        secondary: orange
+      }
     }
   }
 })
