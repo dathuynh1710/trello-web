1df6f703f86508d72491e2f12748b5ef3b25c1ec
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-08-11 17:59:09 +0700

    Add call API

---
+++ src/apis/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+import { API_ROOT } from '~/utils/constants'
+export const fetchBoardDetailsAPI = async (boardId) => {
+  const response = await axios.get(`${API_ROOT}/v1/boards/${boardId}`)
+  // Axios trả về qua pros của nó là data
+  return response.data
+}
