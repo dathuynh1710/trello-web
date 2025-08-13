86f75790613fcc2a15b3bb97d55b25fe62dcabe1
dathuynh1710 <huynhthanhdat2506@gmail.com>
2025-08-13 23:41:45 +0700

    Add drag-and-drop columns integration with API

--- src/apis/index.js
+++ src/apis/index.js
@@ -8,6 +8,12 @@ export const fetchBoardDetailsAPI = async (boardId) => {
   return response.data
 }
 
+export const updateBoardDetailsAPI = async (boardId, updateData) => {
+  const response = await axios.put(`${API_ROOT}/v1/boards/${boardId}`, updateData)
+  // Axios trả về qua pros của nó là data
+  return response.data
+}
+
 // Columns
 export const createNewColumnAPI = async (newColumnData) => {
   const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
